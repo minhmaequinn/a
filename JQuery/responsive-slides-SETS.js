802a862f4603eb1ab938a6eea35f7d9f06090758
minhmaequinn <150760494+minhmaequinn@users.noreply.github.com>
2023-11-15 23:31:03 -0600

    Add files via upload

---
+++ JQuery/responsive-slides-SETS.js
@@ -0,0 +1,48 @@
+// responsiveSlides - Allwebco Version 1.1 2016
+// ------------------------------
+
+
+
+
+
+// START CONTENT 1 HOMEPAGE --------------
+
+    $(function () {
+
+      // Slideshow 1
+      $("#slider1").responsiveSlides({
+        auto: true,
+        pager: true,
+        nav: true,
+        speed: 500,
+        timeout: 9000,
+        maxwidth: 3000,
+        namespace: "centered-btns"
+      });
+
+    });
+
+// END CONTENT 1 HOMEPAGE --------------
+
+
+
+
+
+// START CONTENT 2 SLIDESHOW SETS --------------
+
+    $(function () {
+
+      // Slideshow 2
+      $("#slider2").responsiveSlides({
+        auto: true,
+        pager: true,
+        nav: true,
+        speed: 500,
+        timeout: 8000,
+        maxwidth: 3000,
+        namespace: "centered-btns"
+      });
+
+    });
+
+// END CONTENT 2 ALL SLIDESHOWS --------------
